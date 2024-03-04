746764d2ae12686f3068ec6ab611e6f388ba5a1e
Yurii Khrystuk <22624868+khrystuk@users.noreply.github.com>
2024-03-04 23:37:30 +0200

    Added bufferline

--- lua/plugins/comments.lua
+++ lua/plugins/comments.lua
@@ -1,6 +1,6 @@
 return {
 	{
-		"numToStr/Comment.nvim",
+		{ "numToStr/Comment.nvim", opts = {} },
 		{ "folke/todo-comments.nvim", dependencies = { "nvim-lua/plenary.nvim" }, opts = { signs = false } },
 	},
 }
